006b8ca936c31fc1a970bf1c3df3b6005d8665c2
vengi001 <vengutu725@gmail.com>
2024-10-13 18:38:20 +0530

    path check

--- bussiness-chatbot/chat.py
+++ bussiness-chatbot/chat.py
@@ -8,12 +8,16 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+import os
 
-with open('intents.json', 'r') as intent_data:
+print("Current Working Directory:", os.getcwd())
+print("Files in Directory:", os.listdir('.'))
+
+with open('bussiness-chatbot/intents.json', 'r') as intent_data:
     intents = json.load(intent_data)
 
 bot_name = "ChatBot"
-model_path = "model.pth"
+model_path = "bussiness-chatbot/model.pth"
 
 data = torch.load(model_path)
 
